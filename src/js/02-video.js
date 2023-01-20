b4786c7486049a2c4f805becc4c875272702e58b
vya-vyu <linda1307@gmail.com>
2023-01-20 19:36:21 +0000

    01-gallary

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,11 @@
+import Player from '@vimeo/player';
 
+const player = new Player('handstick', {
+    id: 19231868,
+    width: 640
+});
+
+player.on('play', function(data) {
+    console.log('played the video!');
+    console.log(data)
+});
